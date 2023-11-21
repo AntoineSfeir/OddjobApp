a5f2bbdff1e7b5d4828c4b3c1b9015fa931779ef
AntoineSfeir <antoinegsfeir@gmail.com>
2023-11-21 11:54:12 -0600

    fixed pixel overflow.

--- odd_job_app/lib/pages/profile_page.dart
+++ odd_job_app/lib/pages/profile_page.dart
@@ -15,7 +15,6 @@ import 'package:odd_job_app/pages/payment_option_page.dart';
 import 'package:odd_job_app/pages/manage_location_page.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
-
 // ignore: unused_import
 
 class ProfilePage extends StatefulWidget {
@@ -32,7 +31,6 @@ class _ProfileState extends State<ProfilePage> {
   final user = FirebaseAuth.instance.currentUser!;
   String? currentUserDocId;
   File? _image; // Variable to store the selected image
- 
 
   Future<void> getUserDocId() async {
     await FirebaseFirestore.instance.collection('users').get().then(
@@ -129,7 +127,6 @@ class _ProfileState extends State<ProfilePage> {
     }
   }
 
-
   Future<String?> getProfilePictureUrl(String documentId) async {
     try {
       final ref = firebase_storage.FirebaseStorage.instance
@@ -162,10 +159,10 @@ class _ProfileState extends State<ProfilePage> {
                 AppBar(
                   backgroundColor: Color(0xFF4F82A3),
                   title: const Text('Profile',
-            style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.bold)),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold)),
                 ),
                 // Profile Picture and Name
                 Row(
@@ -199,28 +196,30 @@ class _ProfileState extends State<ProfilePage> {
                         //           fit: BoxFit.cover,
                         //         ),
                         // ),
-                       child: FutureBuilder<String?>(
-      future: getProfilePictureUrl(currentUserDocId!),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(child: CircularProgressIndicator());
-        } else if (snapshot.hasError || snapshot.data == null) {
-          return CircleAvatar(
-            backgroundColor: Color(0xFFC9D0D4),
-          );
-        } else {
-          return Center(
-            child: CircleAvatar(
-              backgroundColor: Color(0xFFC9D0D4),
-              backgroundImage: NetworkImage(snapshot.data!),
-              radius: 50,
-            ),
-          );
-        }
-      },
-    ),
-  ),
-),
+                        child: FutureBuilder<String?>(
+                          future: getProfilePictureUrl(currentUserDocId!),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return Center(child: CircularProgressIndicator());
+                            } else if (snapshot.hasError ||
+                                snapshot.data == null) {
+                              return CircleAvatar(
+                                backgroundColor: Color(0xFFC9D0D4),
+                              );
+                            } else {
+                              return Center(
+                                child: CircleAvatar(
+                                  backgroundColor: Color(0xFFC9D0D4),
+                                  backgroundImage: NetworkImage(snapshot.data!),
+                                  radius: 50,
+                                ),
+                              );
+                            }
+                          },
+                        ),
+                      ),
+                    ),
                     // Display username
                     Container(
                       margin: EdgeInsets.only(
@@ -269,36 +268,39 @@ class _ProfileState extends State<ProfilePage> {
                 ),
                 // Options List
                 const SizedBox(height: 10),
-                SizedBox(height: 22.0),
                 // Manage Profile Information
                 TextButton(
-  onPressed: () {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => const ProfileInfoPage()),
-    );
-  },
-  child: Container(
-    alignment: Alignment.centerLeft,
-    child: Row(
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(right: 8.0), // Added padding
-          child: Icon(Icons.person, size: 20, color: Color(0xFF2598D7)),
-        ),
-        SizedBox(
-          width: 5,
-        ),
-        Text(
-          'Profile Info',
-          style: TextStyle(fontSize: 20, color: Color(0xFF2598D7)),
-        ),
-      ],
-    ),
-  ),
-),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const ProfileInfoPage()),
+                    );
+                  },
+                  child: Container(
+                    alignment: Alignment.centerLeft,
+                    child: Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              right: 8.0), // Added padding
+                          child: Icon(Icons.person,
+                              size: 20, color: Color(0xFF2598D7)),
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          'Profile Info',
+                          style:
+                              TextStyle(fontSize: 20, color: Color(0xFF2598D7)),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
 
-SizedBox(height: 22.0),
+                SizedBox(height: 22.0),
                 // Job history
                 TextButton(
                   onPressed: () {
@@ -312,19 +314,25 @@ SizedBox(height: 22.0),
                     alignment: Alignment.centerLeft,
                     child: const Row(
                       children: <Widget>[
-                                Padding(
-          padding: const EdgeInsets.only(right: 8.0), // Added padding
-          child: Icon(Icons.history, size: 20, color: Color(0xFF2598D7)),
-        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              right: 8.0), // Added padding
+                          child: Icon(Icons.history,
+                              size: 20, color: Color(0xFF2598D7)),
+                        ),
                         SizedBox(
                           width: 5,
                         ),
-                        Text('Job History', style: TextStyle(fontSize: 20, color: Color(0xFF2598D7)), ),
+                        Text(
+                          'Job History',
+                          style:
+                              TextStyle(fontSize: 20, color: Color(0xFF2598D7)),
+                        ),
                       ],
                     ),
                   ),
                 ),
-SizedBox(height: 22.0),
+                SizedBox(height: 16.0),
                 // Payment option
                 TextButton(
                   onPressed: () {
@@ -338,20 +346,26 @@ SizedBox(height: 22.0),
                     alignment: Alignment.centerLeft,
                     child: const Row(
                       children: <Widget>[
-                               Padding(
-          padding: const EdgeInsets.only(right: 8.0), // Added padding
-          child: Icon(Icons.payment, size: 20, color: Color(0xFF2598D7)),
-        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              right: 8.0), // Added padding
+                          child: Icon(Icons.payment,
+                              size: 20, color: Color(0xFF2598D7)),
+                        ),
                         SizedBox(
                           width: 5,
                         ),
-                        Text('Manage Payment Options', style: TextStyle(fontSize: 20, color: Color(0xFF2598D7)),),
+                        Text(
+                          'Manage Payment Options',
+                          style:
+                              TextStyle(fontSize: 20, color: Color(0xFF2598D7)),
+                        ),
                       ],
                     ),
                   ),
                 ),
-SizedBox(height: 22.0),
-                  // Manage Address Info
+                SizedBox(height: 16.0),
+                // Manage Address Info
                 TextButton(
                   onPressed: () {
                     Navigator.push(
@@ -364,19 +378,25 @@ SizedBox(height: 22.0),
                     alignment: Alignment.centerLeft,
                     child: const Row(
                       children: <Widget>[
-                               Padding(
-          padding: const EdgeInsets.only(right: 8.0), // Added padding
-          child: Icon(Icons.location_pin, size: 20, color: Color(0xFF2598D7)),
-        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              right: 8.0), // Added padding
+                          child: Icon(Icons.location_pin,
+                              size: 20, color: Color(0xFF2598D7)),
+                        ),
                         SizedBox(
                           width: 5,
                         ),
-                        Text('Manage Location Settings', style: TextStyle(fontSize: 20, color: Color(0xFF2598D7)),),
+                        Text(
+                          'Manage Location Settings',
+                          style:
+                              TextStyle(fontSize: 20, color: Color(0xFF2598D7)),
+                        ),
                       ],
                     ),
                   ),
                 ),
-SizedBox(height: 22.0),
+                SizedBox(height: 16.0),
                 // About OddJob
                 TextButton(
                   onPressed: () {
@@ -390,19 +410,27 @@ SizedBox(height: 22.0),
                     alignment: Alignment.centerLeft,
                     child: const Row(
                       children: <Widget>[
-                                Padding(
-          padding: const EdgeInsets.only(right: 8.0), // Added padding
-          child: Icon(Icons.info, size: 20, color: Color(0xFF2598D7)),
-        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              right: 8.0), // Added padding
+                          child: Icon(Icons.info,
+                              size: 20, color: Color(0xFF2598D7)),
+                        ),
                         SizedBox(
                           width: 5,
                         ),
-                        Text('About OddJob', style: TextStyle(fontSize: 20, color: Color(0xFF2598D7),),),
+                        Text(
+                          'About OddJob',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Color(0xFF2598D7),
+                          ),
+                        ),
                       ],
                     ),
                   ),
                 ),
-SizedBox(height: 22.0),
+                SizedBox(height: 16.0),
                 // logout button
                 TextButton(
                   onPressed: () {
@@ -417,15 +445,20 @@ SizedBox(height: 22.0),
                     child: const Row(
                       children: <Widget>[
                         Padding(
-                          padding: EdgeInsets.only(
-                              right: 8.0), // Added padding
+                          padding: EdgeInsets.only(right: 8.0), // Added padding
                           child: Icon(Icons.logout,
                               size: 18, color: Color(0xFF2598D7)),
                         ),
                         SizedBox(
                           width: 5,
                         ),
-                        Text('Logout', style: TextStyle(fontSize: 20, color: Color(0xFF2598D7),),),
+                        Text(
+                          'Logout',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Color(0xFF2598D7),
+                          ),
+                        ),
                       ],
                     ),
                   ),
