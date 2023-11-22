56b664f30d4ad2ae468e5eb844a9c6ac3bd5032d
AntoineSfeir <102171038+AntoineSfeir@users.noreply.github.com>
2023-11-21 20:36:19 -0600

    make job description page look a lil better.

--- odd_job_app/lib/jobs/jobdescription.dart
+++ odd_job_app/lib/jobs/jobdescription.dart
@@ -52,258 +52,253 @@ class _JobDescriptionPageState extends State<JobDescriptionPage> {
 
     allJobs(); // Call the method in initState
   }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Job Details'),
-        backgroundColor: const Color(0xFF4F83A2), // Set the desired color
-        elevation: 0, // Remove app bar shadow
-      ),
-      body: FutureBuilder(
-        future: allJobs(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            if (thisJob != null) {
-              return SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Container(
-                      height: 165, // Adjusted height
-                      margin: const EdgeInsets.all(16),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(16),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 3,
-                            blurRadius: 7,
-                            offset: const Offset(0, 3),
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text('Job Details'),
+      backgroundColor: const Color(0xFF4F83A2),
+      elevation: 0,
+    ),
+    body: FutureBuilder(
+      future: allJobs(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (thisJob != null) {
+            return SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    height: 200, // Adjusted height
+                    margin: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 3,
+                          blurRadius: 7,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: GoogleMap(
+                      initialCameraPosition:
+                          CameraPosition(target: l, zoom: 18),
+                      mapType: MapType.normal,
+                      myLocationButtonEnabled: true,
+                      compassEnabled: true,
+                      markers: _markers,
+                      onMapCreated: (GoogleMapController controller) {
+                        _mapController = controller;
+                        _markers.add(Marker(
+                          markerId: const MarkerId('jobMarker'),
+                          visible: true,
+                          position: l,
+                          infoWindow: InfoWindow(
+                            title:
+                                '${computedDistance.compute(thisJob.longlat, thisJob.longlat).toString()} miles',
                           ),
-                        ],
-                      ),
-                      child: GoogleMap(
-                        initialCameraPosition:
-                            CameraPosition(target: l, zoom: 18),
-                        mapType: MapType.normal,
-                        myLocationButtonEnabled: true,
-                        compassEnabled: true,
-                        markers: _markers,
-                        onMapCreated: (GoogleMapController controller) {
-                          _mapController = controller;
-                          _markers.add(Marker(
-                            markerId: const MarkerId('jobMarker'),
-                            visible: true,
-                            position: l,
-                            infoWindow: InfoWindow(
-                              title:
-                                  '${computedDistance.compute(thisJob.longlat, thisJob.longlat).toString()} miles',
+                        ));
+                        _mapController.animateCamera(
+                          CameraUpdate.newLatLng(l),
+                        );
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  OtherProfilePage(recieverUser: thisUser),
                             ),
-                          ));
-                          _mapController.animateCamera(
-                            CameraUpdate.newLatLng(l),
                           );
                         },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              const Color(0xFF2598D7)),
+                          shape: MaterialStateProperty.all<
+                              RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                          ),
+                        ),
+                        child: Text(
+                          thisJob.displayName,
+                          style: const TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Align(
-                        alignment: Alignment.topLeft,
-                        child: ElevatedButton(
+                  ),
+                  buildStarRating(avgUserRating),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Text(
+                        "${(avgUserRating / 2).round()}/5 stars based on ${5} ratings",
+                        style: const TextStyle(
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Center(
+                      child: Text(
+                        "Time Remaining: ${computedTime.compute(thisJob.deadline)}",
+                        style: const TextStyle(
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 16.0),
+                    padding: const EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 1.0,
+                      ),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          thisJob.title,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                        const SizedBox(height: 8.0),
+                        const Divider(
+                          color: Colors.black,
+                          thickness: 1.0,
+                        ),
+                        const SizedBox(height: 8.0),
+                        Text(
+                          thisJob.description,
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(height: 16.0),
+                        Text(
+                          "Current Bid: \$${thisJob.startingBid}",
+                          style: const TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 16.0),
+                        ElevatedButton(
                           onPressed: () {
                             Navigator.push(
                               context,
                               MaterialPageRoute(
-                                builder: (context) =>
-                                    OtherProfilePage(recieverUser: thisUser),
+                                builder: (context) => BidPage(
+                                  stringBid: thisJob.startingBid,
+                                  jobID: thisJob.ID,
+                                ),
                               ),
                             );
                           },
                           style: ButtonStyle(
                             backgroundColor: MaterialStateProperty.all<Color>(
-                                const Color(0xFF2598D7)),
-                            shape: MaterialStateProperty.all<
-                                RoundedRectangleBorder>(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20.0),
-                              ),
+                                const Color(0xFF1D465D)),
+                            minimumSize: MaterialStateProperty.all<Size>(
+                              const Size(
+                                  200, 50), // Adjust the height as needed
+                            ),
+                            padding:
+                                MaterialStateProperty.all<EdgeInsetsGeometry>(
+                              const EdgeInsets.symmetric(horizontal: 24),
                             ),
                           ),
-                          child: Text(
-                            thisJob.displayName,
-                            style: const TextStyle(
-                              fontSize: 24.0,
-                              fontWeight: FontWeight.bold,
+                          child: const Text(
+                            'Bid Now',
+                            style: TextStyle(
                               color: Colors.white,
+                              fontSize: 20.0,
                             ),
                           ),
                         ),
-                      ),
+                      ],
                     ),
-
-                    buildStarRating(avgUserRating),
-
-
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: Text(
-                          //"${(avgUserRating/2).round()}/5 stars based on ${5} ratings",
-                          "${(avgUserRating/2).round()}/5 stars based on ${5} ratings",
-                          style: const TextStyle(
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.blue,
-                            decoration: TextDecoration.underline,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Center(
-                        child: Text(
-                          "Time Remaining: ${computedTime.compute(thisJob.deadline)}",
-                          style: const TextStyle(
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-
-                    const SizedBox(height: 16), // Increased spacing
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 16.0),
-                      padding: const EdgeInsets.all(16.0),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.black,
-                          width: 1.0,
-                        ),
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            thisJob.title,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16.0,
-                            ),
-                          ),
-                          const SizedBox(height: 8.0),
-                          const Divider(
-                            color: Colors.black,
-                            thickness: 1.0,
-                          ),
-                          const SizedBox(height: 8.0),
-                          Text(
-                            thisJob.description,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16.0,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(height: 16), // Increased spacing
-                    Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const SizedBox(height: 16.0), // Padding above
-                          Text(
-                            "Current Bid: \$${thisJob.startingBid}",
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 24.0, // Adjusted font size
-                            ),
-                          ),
-                          const SizedBox(height: 16.0), // Padding below
-
-                          const SizedBox(height: 16.0), // Increased spacing
-                          ElevatedButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => BidPage(
-                                    stringBid: thisJob.startingBid,
-                                    jobID: thisJob.ID,
-                                  ),
-                                ),
-                              );
-                            },
-                            style: ButtonStyle(
-                              backgroundColor: MaterialStateProperty.all<Color>(
-                                  const Color(0xFF1D465D)),
-                              minimumSize: MaterialStateProperty.all<Size>(
-                                const Size(
-                                    200, 50), // Adjust the height as needed
-                              ),
-                              padding:
-                                  MaterialStateProperty.all<EdgeInsetsGeometry>(
-                                const EdgeInsets.symmetric(horizontal: 24),
-                              ),
-                            ),
-                            child: const Text(
-                              'Bid Now',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 24.0,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            } else if (snapshot.hasError) {
-              return Center(
-                child: Text(snapshot.error.toString()),
-              );
-            } else {
-              return const Text('Something went wrong');
-            }
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
+                  ),
+                ],
+              ),
             );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text(snapshot.error.toString()),
+            );
+          } else {
+            return const Text('Something went wrong');
           }
-        },
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    ),
+  );
+}
+
+Widget buildStarRating(int userRating) {
+  List<Icon> stars = [];
+  userRating = (userRating / 2).round();
+
+  for (int i = 0; i < 5; i++) {
+    stars.add(
+      Icon(
+        Icons.star,
+        color: i < userRating ? Colors.amber : Colors.grey,
+        size: 28, // Adjusted star size
       ),
     );
   }
 
-  Widget buildStarRating(int userRating) {
-    List<Icon> stars = [];
-    userRating = (userRating / 2).round();
+  return Padding(
+    padding: const EdgeInsets.only(
+      left: 15.0,
+      top: 8.0, // Added top padding for spacing
+    ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: stars,
+    ),
+  );
+}
 
-    for (int i = 0; i < 5; i++) {
-      stars.add(
-        Icon(
-          Icons.star,
-          color: i < userRating ? Colors.yellow : Colors.grey,
-        ),
-      );
-    }
-
-    return Padding(
-      padding: const EdgeInsets.only(
-          left: 15.0), // Adjust the left padding as needed
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: stars,
-      ),
-    );
-  }
 }
